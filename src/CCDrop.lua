15e31f1c0ad31b35c1ff7b6888dee45a963bda39
hbomb79 <harryfelton12@gmail.com>
2017-05-07 13:45:50 +1200

    Added Notification dropdown (tiled)

--- src/CCDrop.lua
+++ src/CCDrop.lua
@@ -1,3 +1,5 @@
+centrePrint("Loading CCDrop", TERM_Y - 1, colours.lightBlue)
+
 -- Bind the custom ModemEvent to 'modem_message' events
 Event.bindEvent( "modem_message", "ModemEvent" )
 
@@ -15,7 +17,12 @@ client:query "#settings":on( "trigger", function() client:revealSettings() end )
 
 -- Cache important nodes
 notificationContent = client:query "#notifications".result[ 1 ]
+notificationView = notificationContent:query "#panel".result[ 1 ]
+notificationChanger = notificationView:query "#panel_changer".result[ 1 ]
+notificationTiler = notificationChanger:query "#list".result[ 1 ]
 pages = client:query "PageContainer#master".result[ 1 ]
+
+notificationChanger:selectPage "main"
 
 -- Let click events pass through the container if they don't collide with a child node
 notificationContent.consumeAll = false
@@ -32,6 +39,15 @@ end)
 client:schedule( function()
     client:addNotification( notif )
 end, 2 )
+
+local notifs_close = client:query "#collapse_notifications".result[ 1 ]
+local notifs_open = client:query "#open_notifications".result[ 1 ]
+
+notifs_close.text = _HOST and "\30" or notifs_close.text
+notifs_close:on("trigger", function() client:closeNotifications() end)
+
+notifs_open.text = _HOST and "\31" or notifs_open.text
+notifs_open:on("trigger", function() client:openNotifications() end)
 
 -- Start the client
 client.state = "root"
--- src/ui/splash.lua
+++ src/ui/splash.lua
@@ -4,12 +4,13 @@ TERM_X, TERM_Y = term.getSize()
 term.setBackgroundColour( colours.grey )
 term.clear()
 
-local function centrePrint( text, y, colour )
+function centrePrint( text, y, colour )
     term.setCursorPos( TERM_X / 2 - #text / 2, y )
     term.setTextColour( colour or 1 )
 
+    term.clearLine()
     term.write( text )
 end
 
 centrePrint("CCDrop", 7)
-centrePrint("Loading... Please wait", TERM_Y - 1, colours.lightBlue)
+centrePrint("Checking Titanium for updates", TERM_Y - 1, colours.lightBlue)
